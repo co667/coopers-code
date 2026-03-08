e2053afb4a4da80d33d5202187757f2b8940d08c
Cooper Friedman <amillionbillionscmf@gmail.com>
2026-03-08 18:26:53 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,51 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Windows":return img`
+f f f f f f f f f 9 9 f f f f f 
+f f f f f f f f 9 9 9 f f f f f 
+f f f f f f f f 9 9 9 f f f f f 
+f f f f f f 9 f 9 9 9 f f f f f 
+f f f f f 9 9 f 9 9 9 f f f f f 
+f f f f 9 9 9 f 9 9 9 f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f 9 9 9 f 9 9 9 f f f f f 
+f f f f f 9 9 f 9 9 9 f f f f f 
+f f f f f f 9 f 9 9 9 f f f f f 
+f f f f f f f f 9 9 9 f f f f f 
+f f f f f f f f f 9 9 f f f f f 
+f f f f f f 1 1 1 f f f f f f f 
+f f f f f f 1 f 1 f f f f f f f 
+f f f f f f 1 1 f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("json", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
